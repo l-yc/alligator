9bd03ac619456ba24467f97e5aaeeb874b0ee4b1
l-yc <22719973+l-yc@users.noreply.github.com>
2020-07-26 16:07:47 +0800

    add server-side routes and dummy data server

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 
+import Data from './Data';
+
 class App {
   public express: any;
+
+  private data: Data;
 
   constructor() {
     // Create a new express application instance
     this.express = express();
+
+    this.data = new Data();
 
     this.config();
     this.mountRoutes();
@@ -21,14 +27,31 @@ class App {
 
   private mountRoutes(): void {
     const router = express.Router();
+    //router.get('/assets/*', function (req: Request, res: Response) {
+    //  res.sendFile(path.join(__dirname, req.url));
+    //});
     router.get('/', function (req: Request, res: Response) {
       res.redirect('/dashboard');
     });
+
     router.get('/dashboard', function (req: Request, res: Response) {
       res.render('dashboard', { title: 'alligator', user: 'admin' }); // TODO: hook this to a login!
     });
-    router.get('/assets/*', function (req: Request, res: Response) {
-      res.sendFile(path.join(__dirname, req.url));
+
+    router.get('/manage/hatchlings', function (req: Request, res: Response) {
+      res.render('manage/hatchlings/main', { title: 'manage' }); // TODO: hook this to a login!
+    });
+
+    router.get('/manage/hatchlings/edit', function (req: Request, res: Response) {
+      res.render('manage/hatchlings/edit', { title: 'edit', id: req.query.id }); // TODO: hook this to a login!
+    });
+
+    router.get('/api/hatchlings/get', (req: Request, res: Response) => {
+      console.log('GET hatchlings');
+      res.status(200).json({
+        hatchlings: this.data.getHatchlings(),
+        success: true
+      });
     });
 
     this.express.use('/', router);
---
+++ src/Data.ts
@@ -0,0 +1,19 @@
+class Data {
+  constructor() {
+  }
+
+  public getHatchlings(): any {
+    return [
+      { id: '0a00c85d531755f41573fc46cd8f3335', name: 'dummy1' },
+      { id: '6b55da91031efccd97bdf7a7bf4280ac', name: 'dummy2' },
+      { id: 'a04109510f428fea59c4554fc0598721', name: 'dummy3' },
+      { id: '21033c1b2b557a30132ec1b6c8a6ab29', name: 'dummy4' },
+      { id: 'fd8c0864e15b5a5479bf96d0196cfb2a', name: 'dummy5' },
+      { id: 'acd615b1a0e9ca174d6d89ea58e1e755', name: 'dummy6' },
+      { id: '413866e257853db42241a2d06bf8a16d', name: 'dummy7' },
+      { id: 'abcfeb8523046956d29f3f7cc19f7b89', name: 'dummy8' }
+    ];
+  }
+}
+
+export default Data;
